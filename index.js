9ce9b7e555c31c0afd8f7c886669c28740c5251f
Hao Yuan <haomichaelyuan@gmail.com>
2022-09-22 11:58:02 +1000

    Done with assignment

---
+++ index.js
@@ -0,0 +1,11 @@
+function receivesAFunction (thing){
+    return thing()
+}
+
+function returnsANamedFunction (){
+    return function aya(){}
+}
+
+function returnsAnAnonymousFunction (){
+    return function(){}
+}
